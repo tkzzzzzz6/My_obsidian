1ea4df634aa1438616723722a7e47db8d3a79f1b
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2025-09-20 21:46:30 +0800

    Update SQL file for student major and add new delete operation; modify workspace to reflect recent changes

--- 学习/计算机三级/数据库/真题题库练习/note/5.select.sql
+++ 学习/计算机三级/数据库/真题题库练习/note/5.select.sql
@@ -1,12 +1,9 @@
--- constrain
-
-SET SQL_SAFE_UPDATES = 0;
-
 CREATE TABLE student(
     'student_id' INT AUTO_INCREMENT,
     'name' VARCHAR(20) NOT NULL,
     'major' VARCHAR(20) DEFAULT 'NULL',
     PRIMARY KEY('student_id') 
+    'score' INT
 )
 
 INSERT INTO 'student' VALUES(1,'小白','计算机科学与技术');
@@ -17,10 +14,11 @@ INSERT INTO 'student'('student_id','name','major') VALUES(2,'小黑','数学');
 INSERT INTO 'student'('student_id','name','major') VALUES(3,'小红','NULL');
 INSERT INTO 'student'('student_id','name') VALUES(4,'小蓝');
 
-UPDATE student
-SET 'major' = '大数据科学与技术'
-WHERE 'major' = '数据应用科学' OR 'major' = '大数据'
+SELECT 'name','major' FROM student ORDER BY 'score','student_id' ASC;
+-- ASC low to high *DEFUALT*
+-- DESC high to low
 
-DELETE FROM student
-WHERE 'student_id' = 2;
-
+SELECT * FROM student ORDER BY 'score' DESC LIMIT 3;
+SELECT * FROM student WHERE'score' <> 80;
+SELECT * FROM student WHERE 'major' IN ('数学','计算机科学与技术','NULL');
+SELECT * FROM student WHERE 'major' = 'NULL' OR 'major' = '计算机科学与技术';
